f9d8b481be8817a3573f605a1c7b2c5f6023205f
Matt Hall <matthew@quickbeam.me.uk>
2023-04-23 20:23:24 +0100

    Add build step to flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,33 @@
 {
-  description = "A TOML library for Zig";
+  description = "A units replacement in Zig";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+
     zig = {
       url = "github:mitchellh/zig-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     zls = {
       url = "github:zigtools/zls";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {self, nixpkgs, flake-utils, flake-compat, zig, zls}:
+  outputs = {self, nixpkgs, gitignore, flake-utils, flake-compat, zig, zls}:
     let
       overlays = [
         (final: prev: { 
@@ -29,20 +38,37 @@
         })
       ];
       systems = builtins.attrNames zig.packages;
+      inherit (gitignore.lib) gitignoreSource;
     in
       flake-utils.lib.eachSystem systems (system:
         let
           pkgs = import nixpkgs { inherit overlays system; };
+          zig = pkgs.zigpkgs.master-2023-04-22;
         in
           rec {
             devShell = pkgs.mkShell {
               buildInputs = (with pkgs; [
-                zigpkgs.master-2023-04-22
                 zlspkgs.default
+                zig
                 bashInteractive
                 gdb
                 lldb
               ]);
+            };
+
+            packages.default = packages.ulice;
+            packages.ulice = pkgs.stdenvNoCC.mkDerivation {
+              name = "ulice";
+              version = "master";
+              src = gitignoreSource ./.;
+              nativeBuildInputs = [ zig ];
+              dontConfigure = true;
+              dontInstall = true;
+              buildPhase = ''
+                mkdir -p $out
+                mkdir -p .cache/{p,z,tmp}
+                zig build install --cache-dir $(pwd)/zig-cache --global-cache-dir $(pwd)/.cache -Dcpu=baseline -Doptimize=ReleaseSafe --prefix $out
+              '';
             };
           }
       );
